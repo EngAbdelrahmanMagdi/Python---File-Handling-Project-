28d02084f62b330ab1c58d787a1c5e7aa672da66
EngAbdelrahmanMagdi <eng.abdelrahman.magdi@gmail.com>
2022-04-16 13:35:54 +0200

    project added

---
+++ view_projects.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from project import menu
+import json
+
+
+def view(user_id):
+
+    mypath = Path("D:\Programming\Python\Python Labs\Day02\Lab02\Crowd Funding Console app/projects_data.json")
+    with open('projects_data.json') as json_file:
+        if mypath.stat().st_size == 0:
+            print("there is no projects")
+        else:
+            list = []
+            json_file = open('projects_data.json')
+            for line in json_file:
+                Dict = json.loads(line)
+                if Dict['project_user_id'] == user_id:
+                    list.append(Dict)
+
+            if len(list) == 0:
+                print("no project for you ")
+                menu(user_id)
+            else:
+                print("your projects :")
+                for dict in list:
+                    print(dict['title'])
+
+
